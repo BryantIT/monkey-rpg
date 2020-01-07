82f99ce7d5fb8c9e7db6d4781964169b0d748852
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-07 04:15:30 -0600

    created character database

--- db/migrate/20200107094744_create_characters.rb
+++ db/migrate/20200107094744_create_characters.rb
@@ -1,6 +1,22 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :health
+      t.integer :strength
+      t.integer :dexterity
+      t.integer :magic
+      t.integer :charisma
+      t.integer :intelligence
+      t.string :job
+      t.string :race
+      t.string :gender
+      t.integer :age
+      t.string :eye_color
+      t.string :hair_color
+      t.string :size
+      t.text :description
 
       t.timestamps
     end
