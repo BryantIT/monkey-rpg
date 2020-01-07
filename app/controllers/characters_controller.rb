f7c3589a7abdd1c1b3ffc73162861f08f1afeb5d
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-07 04:47:14 -0600

    set up character crud

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,6 +9,40 @@ class CharactersController < ApplicationController
   end
 
   def create
+    @character = current_user.characters.build(character_params)
+    @character.user_id = current_user.id
 
+    if @character.save
+      redirect_to @character
+    else
+      render :new
+    end
+  end
+
+  def show
+    @character = Character.find_by_id(params[:id])
+  end
+
+  def edit
+    @character = Character.find_by_id(params[:id])
+  end
+
+  def update
+    @character = Character.find_by_id(params[:id])
+    @character.update(character_params)
+    redirect_to character_path(@character)
+  end
+
+  def destroy
+    @character = Character.find_by_id(params[:id])
+    @character.destroy
+    redirect_to characters_path
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:first_name, :last_name, :health, :strength,
+      :dexterity, :magic, :charisma, :intelligence, :job, :race, :gender, :age,
+      :eye_color, :hair_color, :size, :description, :user_id, :avatar)
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ApplicationRecord
+  has_one_attached :avatar
+  belongs_to :user
 end
